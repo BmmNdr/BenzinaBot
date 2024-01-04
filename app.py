c253e9513e879eb7fda5f21fc5dd1258dce7ea8e
BmmNdr <abiemmibi@gmail.com>
2024-01-04 11:48:38 +0100

    Added update fuel type command

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import requests
 from time import sleep
+from telegramBot import telegramBot
 
+naftaBot = telegramBot()
+
+while(True):
+    messages = naftaBot.getUpdates()
+    
+    for msg in messages:
+        chat_id = msg["message"]["chat"]["id"]
+        text = msg["message"]["text"]
+        
+        if(text == "/start"):
+            naftaBot.sendMessage(chat_id, "Benvenuto nel Nafta bot")
+        elif(text == "/aggiornacarburante"):
+            naftaBot.UpdateFuelType(chat_id)
+        elif(text == "/aggiornacapienza"):
+            naftaBot.UpdateFuelCap(chat_id)
+        elif(text == "/rifornimento"):
+            naftaBot.Refuel(chat_id)
+        
+        #else: #crea problema con getReply
+        #    naftaBot.sendMessage(chat_id, "Comando non riconosciuto")
+    
+    sleep(5)
--- telegramBot.py
+++ telegramBot.py
@@ -1,13 +1,15 @@
 import requests
+import time
 
-BOT_TOKEN = "6383738627:AAEzW1LNl3v3q9PLOxjMChI9F1X2hEkBmsA"
+BOT_TOKEN = "bot6450047602:AAHYA1jfTMs0DQ94YUi1NFex52v3tRMMLbo"
 
 class telegramBot:
     
     def __init__(self):
         self.url = f"https://api.telegram.org/{BOT_TOKEN}"
         self.offset = 0
-        
+    
+    #get new messages received by the bot
     def getUpdates(self):
         params = {"offset":self.offset} #first update id to visualize
         resp = requests.get(self.url+"/getUpdates", params=params)
@@ -18,6 +20,54 @@ class telegramBot:
             
         return data['result']
     
+    #gets a message from a specific chat (for 30s)
+    def getReply(self, chat_id): 
+        lastUpdateID = self.offset #internal offset to get only new messages
+        
+        start_time = time.time()
+
+        while time.time() - start_time < 30:
+            params = {"offset":lastUpdateID}
+            resp = requests.get(self.url+"/getUpdates", params=params)
+            
+            data = resp.json()
+            
+            for e in data["result"]:
+                lastUpdateID = e["update_id"] + 1
+                
+                if(e["message"]["chat"]["id"] == chat_id):
+                    return e["message"]["text"]
+                
+        return None
+    
+    #send a message to a specific chat
     def sendMessage(self, chat_id, text):
+        params = {"chat_id":chat_id, "text":text}
+        requests.post(self.url+"/sendMessage", params=params)
+        
+    #update the type of fuel
+    def UpdateFuelType(self, chat_id):
+        self.sendMessage(chat_id, "Inserire il nuovo tipo di carburante")
+        
+        reply = self.getReply(chat_id)
+        
+        if(reply != None):
+            print(reply) #TODO: save in db
+        else:
+            self.sendMessage(chat_id, "Hai impiegato troppo tempo per inserire il tipo di carburante. Riprova il comando")
+            
+    #update the capacity of the tank
+    def UpdateFuelCap(self, chat_id):
+        self.sendMessage(chat_id, "Inserire la nuova capienza del serbatoio")
+        
+        reply = self.getReply(chat_id)
+        
+        if(reply != None):
+            print(reply) #TODO: save in db
+        else:
+            self.sendMessage(chat_id, "Hai impiegato troppo tempo per inserire la nuova capienza. Riprova il comando")
+        
+    #make a refueling
+    def Refuel(self, chat_id): #TODO
         params = {"chat_id":chat_id, "text":text}
         requests.post(self.url+"/sendMessage", params=params)
