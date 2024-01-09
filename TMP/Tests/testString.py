3e60b056b06ff40f3daf7c7ff8620ca695bdf15e
BmmNdr <abiemmibi@gmail.com>
2024-01-09 18:18:28 +0100

    Added README

--- TMP/Tests/testString.py
+++
@@ -1,9 +0,0 @@
-def starts_with_hello(string):
-    if string.startswith("Hello"):
-        return True
-    else:
-        return False
-
-# Example usage
-print(starts_with_hello("Hello, world!"))  # Output: True
-print(starts_with_hello("Hi there!"))      # Output: False
